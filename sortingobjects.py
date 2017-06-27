e2bdaded3077880a531d12aa8ebc69b1bfa98b87
Sumeet Tulsani <sumeettulsani1@gmail.com>
2017-06-27 14:15:40 -0700

    Sorting custom objects

---
+++ sortingobjects.py
@@ -0,0 +1,16 @@
+from operator import attrgetter
+
+class User:
+    def __init__(self,user_id):
+        self.user_id = user_id
+
+    def __repr__(self):
+        return 'User({})'.format(self.user_id)
+
+users = [User(23), User(3), User(99)]
+
+print(users)
+
+usersorted = sorted(users, key=attrgetter('user_id'))
+
+print(usersorted)
