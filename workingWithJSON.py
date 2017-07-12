e279fa060e21db23c1abf60915193ab654d68a7f
Sumeet Tulsani <sumeettulsani1@gmail.com>
2017-07-12 14:23:09 -0700

    Worked with JSON data

---
+++ workingWithJSON.py
@@ -0,0 +1,16 @@
+import json
+from pprint import pprint
+
+data = {
+    'name' : 'Sumeet',
+    'shares' : 2000,
+    'value' : 5000
+}
+
+json_str = json.dumps(data)
+
+pprint(json_str)
+
+data2 = json.loads(json_str)
+
+print(data2)
